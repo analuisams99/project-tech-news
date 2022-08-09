e006d86636f76a11179ec3eb5d2f4d4271288d5c
Ana Luisa <ana.luisa.isa@hotmail.com>
2022-08-09 19:52:31 -0300

    Cria a função search_by_title para retornar uma lista de tuplas com as notícias encontradas nesta busca.

--- tech_news/analyzer/search_engine.py
+++ tech_news/analyzer/search_engine.py
@@ -1,6 +1,15 @@
+from tech_news.database import search_news
+
+
 # Requisito 6
 def search_by_title(title):
-    """Seu código deve vir aqui"""
+    news_found = []
+    news_list = search_news({"title": {"$regex": title, "$options": "i"}})
+
+    for news in news_list:
+        news_found.append((news["title"], news["url"]))
+
+    return news_found
 
 
 # Requisito 7
