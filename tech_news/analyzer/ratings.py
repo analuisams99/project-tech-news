6952e90ca29b2406f29a5f5983f8640391cd3c66
Ana Luisa <ana.luisa.isa@hotmail.com>
2022-08-09 21:13:12 -0300

    Cria a função top_5_news, retornando as notícias por ordem de popularidade

--- tech_news/analyzer/ratings.py
+++ tech_news/analyzer/ratings.py
@@ -1,6 +1,19 @@
+from tech_news.database import find_news
+
+
 # Requisito 10
 def top_5_news():
-    """Seu código deve vir aqui"""
+    top_news = []
+    news_list = find_news()
+
+    sorted_news_list = sorted(
+        news_list, key=lambda k: k["comments_count"], reverse=True
+    )
+
+    for new in sorted_news_list[:5]:
+        top_news.append((new["title"], new["url"]))
+
+    return top_news
 
 
 # Requisito 11
